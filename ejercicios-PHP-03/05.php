db0d1329200affeeba384bdab7e03007bf502282
Jesus Martinez Tripiana <jesusmartineztripiana@hotmail.es>
2020-10-29 18:16:47 +0100

    Ampliacion de ejercicios 1

---
+++ ejercicios-PHP-03/05.php
@@ -0,0 +1,65 @@
+<?php 
+    function generarNumero(array $numeros):int {
+        $repetido = true;
+        $num = 0;
+        while ($repetido){
+            $num=random_int(1,49);
+            $repetido = comprobarRepetido($num,$numeros);
+        }
+       return $num;
+    }
+    
+    function comprobarRepetido(int $num, array $valores):bool {
+        $esta=false;
+        foreach ($valores as $valor){
+            if($valor == $num){
+                $esta=true;
+            }
+        }
+        return $esta;
+    }
+?>
+
+
+
+<?php 
+define ('PARTIDA',6);
+$bonoloto =[];
+for ($i=0;$i<PARTIDA;$i++){
+    $bonoloto [] = generarNumero($bonoloto);
+}
+$complementario = $bonoloto[count($bonoloto)-1];
+asort($bonoloto);
+?>
+
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <style type="text/css">
+        table, td{
+          border: 2px solid black;
+          border-collapse:collapse;
+          margin:3px;
+        }
+        td{
+            padding:5px;
+            color:blue;
+        }
+    </style>
+  </head>
+  <body>
+    <h1>Ejercicio 5</h1>
+	<hr>
+	<b>Juego de la BONOLOTO</b>
+	<table>
+		<tr>
+		<?php foreach ($bonoloto as $clave => $valor){
+		    if ((count($bonoloto)-1)!=$clave){
+		        echo '<td>'.$valor.'</td>';
+		    }
+		}?> 	
+		<td><?= 'Complementario '.$complementario?></td>
+		</tr>
+	</table>
+  </body>
+</html>
