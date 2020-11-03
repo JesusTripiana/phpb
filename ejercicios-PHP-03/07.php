3df3c68a0f952cb44170d1afe08be1831b762186
JesusTripiana <jesusmartineztripiana@hotmail.es>
2020-11-03 09:21:14 +0100

    Ejercicio 07 PHP03

--- ejercicios-PHP-03/07.php
+++ ejercicios-PHP-03/07.php
@@ -4,24 +4,24 @@
 	<title>Datos del Pais</title>
 </head>
 <body> 
-	<?php 
-	   include_once 'infoPaises.php';
-	   
-	   $paisAleatorio = array_rand($paises,1);
-	   
-	   foreach ($paisAleatorio as $pais){
-	       echo "País : ".$pais." , con ".number_format($paises[$pais]['Poblacion'], 0, ',', '.'). " habitantes <br/>";
-	       echo "Ciudades: ";
-	       foreach($ciudades[$pais] as $ciudad){
-	           echo $ciudad." ";
-	       }
-	       echo "<br/>Enlace a google maps: ";
-	       ?>
-	       
-    <a href="https://www.google.es/maps/place/<?= $pais?>">Maps</a><br>
-    <?php
-        }
-    ?>  
+<?php 
+	include_once 'infoPaises.php';
+
+$paisAleatorio = array_rand($paises,2);
+
+foreach ($paisAleatorio as $pais){
+    echo "País : ".$pais." , con ".number_format($paises[$pais]['Poblacion'], 0, ',', '.'). " habitantes <br/>";
+    echo "Ciudades: ";
+    foreach($ciudades[$pais] as $ciudad){
+        echo $ciudad." ";
+    }
+    echo "<br/>Vínculo a google maps: ";
+?>
+	<a href="https://www.google.es/maps/place/<?= $pais?>">Maps</a><br>
+<?php
+break;
+}
+?>	 
 
 </body>
 </html>
