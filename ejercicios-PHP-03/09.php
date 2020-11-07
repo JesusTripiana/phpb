4a1ac009ca9238ce4463bd4587819d303465a3eb
Jesus Martinez Tripiana <jesusmartineztripiana@hotmail.es>
2020-11-07 20:06:13 +0100

    Ejercicios PHP-03 Terminados

--- ejercicios-PHP-03/09.php
+++ ejercicios-PHP-03/09.php
@@ -2,14 +2,31 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Paises y ciudades</title>
+		<style type="text/css">
+            table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            }
+         </style>
 	</head>
 	<body> 
     	<h1>Ejercicio 9</h1>
 		<hr>
+		<h1> Tabla de temperaturas </h1>
+		<table style='border: 1px solid black; border-collapse: collapse;'>
 		
 		<?php 
 		$temperaturas = [ 6, 10, 12, 14,16 ,20 ,25 , 30, 18, 15, 14, 8];
-		$meses = ['enero','febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
-		?>
+		$meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+		$mesTemperatura = array_combine($meses,$temperaturas); // combino los arrays con la funcion mediante 1 array KEY y 2 array VALOR
+		foreach ($mesTemperatura as $mes => $temp){ ?>
+    		<tr><td> <?= $mes ?></td><td>
+    		<?php 
+    		for ($i = 0; $i < $temp; $i++) {
+            echo "<img src ='./imagenes/cuadradoRojo.png' style = width:14px;>";
+            }
+            echo  "  ".$temp. " ºC </td></tr>";
+        } ?>
+		</table>
 	</body>
 </html>
